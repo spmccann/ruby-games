d68bbb6ec9bf6a76e850d64996a73c2cccbc2a7e
Scott <scottpmccann@gmail.com>
2022-05-05 21:03:34 -0700

    data, left and right node. on recursion part

--- BST/binary_search_trees.rb
+++ BST/binary_search_trees.rb
@@ -9,10 +9,10 @@ class Node
     data <=> other.data
   end
 
-  def initialize(data, left, right)
+  def initialize(data)
     @data = data
-    @left = left
-    @right = right
+    @left = nil
+    @right = nil
   end
 end
 
@@ -20,13 +20,27 @@ end
 class Tree
   attr_accessor :array, :root
 
-  def initialize
-    @array = []
-    @root = build_tree
+  def initialize(array)
+    @array = array.sort.uniq
+    # @root = build_tree(array)
   end
 
-  def build_tree(data)
-    'empty' if data.nil?
-    data.uniq!
+  def build_tree(array)
+    nil if array.nil?
+    mid = array.length / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array[0..mid])
+    root.right = build_tree(array[mid + 1..-1])
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+birch = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+birch.pretty_print
