aebca09e6978aac5e4091aaf2739b725e9d19c12
Scott <scottpmccann@gmail.com>
2022-05-07 22:05:15 -0700

    insert

--- BST/main.rb
+++ BST/main.rb
@@ -4,4 +4,6 @@ require_relative 'node'
 require_relative 'tree'
 
 birch = Tree.new([1, 2, 3, 4, 5, 6, 7])
+birch.insert(8)
+birch.delete(6)
 birch.pretty_print
--- BST/tree.rb
+++ BST/tree.rb
@@ -3,18 +3,45 @@
 # tree
 class Tree
   def initialize(array)
-    array = array.sort.uniq
     @root = build_tree(array)
   end
 
   def build_tree(array)
+    array = array.sort.uniq
     return nil unless array
+
     mid = array.length / 2
-    return root = Node.new(array[mid-1]) if array.length < 2
+    return Node.new(array[mid - 1]) if array.length < 2
+
     root = Node.new(array[mid])
     root.left = build_tree(array[0..mid - 1])
-    root.right = build_tree(array[mid + 1..-1]) 
-    p root
+    root.right = build_tree(array[mid + 1..-1])
+    root
+  end
+
+  def insert(key, node = @root)
+    node = Node.new(key) if node.nil?
+    if node.data == key
+      node
+    elsif node.data < key
+      node.right = insert(key, node.right)
+    else
+      node.left = insert(key, node.left)
+    end
+    node
+  end
+
+  def delete(key, node = @root)
+    if node.nil?
+      'key not found'
+    elsif node.data < key
+      node.right = delete(key, node.right)
+    elsif node.data > key
+      node.left = delete(key, node.left)
+    elsif node.data == key
+      node = nil
+    end
+    node
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
