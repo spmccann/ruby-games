1b11cebdf5cd1d8206326d15263823b4d0cd4ce3
Scott <scottpmccann@gmail.com>
2022-05-14 22:23:45 -0700

    remove useless lines

--- knights_travails/tree.rb
+++ knights_travails/tree.rb
@@ -2,12 +2,7 @@
 
 # knight piece
 class Tree
-  include Comparable
   attr_accessor :position, :one, :two, :three, :four, :five, :six
-
-  def <=>(other)
-    position <=> other.position
-  end
 
   def initialize(position)
     @position = position
